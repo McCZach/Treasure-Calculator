15c1589f159c6cf59b6d523829f5c5a5115ca852
Zach McColgan <99850300+McCZach@users.noreply.github.com>
2025-03-26 11:43:16 -0500

    restructure code

--- gui.py
+++ gui.py
@@ -10,87 +10,112 @@ from tkinter import font
 
 class Application():
     def __init__(self):
-        self.__item_counts = {"Total":0, "Potion":0, "Ring":0, "Scroll":0, "Wand":0, "Armor":0}
+        #****************************************************************************************
 
-        self.__main_window = tk.Tk()
-        self.__main_window.geometry('1000x800')
-        self.__main_window.title('Treasure Generator')
+        self.main_window = tk.Tk()
+        self.main_window.geometry('1000x800')
+        self.main_window.title('Treasure Generator')
 
-        # Setup Application
-        self.__option_frame = tk.Frame(self.__main_window)
-        self.__option_frame.pack()
+        self.option_frame = tk.Frame(self.main_window)
+        self.option_frame.pack()
 
-        self.__display_frame = tk.Frame(self.__main_window, border=6, relief='sunken')
-        self.__display_frame.pack()
+        self.display_frame = tk.Frame(self.main_window, border=6, relief='sunken')
+        self.display_frame.pack()
 
-        self.__display_text = scrolledtext.ScrolledText(self.__display_frame, height=30, width=100)
-        self.__display_font = ("Courier", 10, "bold")
-        self.__display_text.tag_configure('red_bg', background='#ff886e', font=self.__display_font)
-        self.__display_text.tag_configure('orange_bg', background='#ffb46e', font=self.__display_font)
-        self.__display_text.tag_configure('yellow_bg', background='#f8ff6e', font=self.__display_font)
-        self.__display_text.tag_configure('green_bg', background='#80ff6e', font=self.__display_font)
-        self.__display_text.tag_configure('blue_bg', background='#6e84ff', font=self.__display_font)
-        self.__display_text.config(state=tk.DISABLED)
-        self.__display_text.pack()
+        self.result_frame = tk.Frame(self.main_window)
+        self.result_frame.pack()
 
-        # Create Result Area
-        self.__result_frame = tk.Frame(self.__main_window)
-        self.__result_frame.pack()
+        #****************************************************************************************
 
-        self.__item_labels = {
-            "Total":tk.Label(self.__result_frame),
-            "Potion":tk.Label(self.__result_frame),
-            "Ring":tk.Label(self.__result_frame),
-            "Scroll":tk.Label(self.__result_frame),
-            "Wand":tk.Label(self.__result_frame),
-            "Armor":tk.Label(self.__result_frame),
-        }
+        self.counts = {'Total':0, 'Potion':0, 'Ring':0, 'Scroll':0, 'Wand':0, 'Armor':0}
 
-        for label in self.__item_labels:
-            self.__item_labels[label].pack()
+        self.create_buttons()
+        self.create_text()
+        self.create_labels()
 
-        self.__buttons = {
-            "Potion":tk.Button(self.__option_frame, text='Generate Potion', 
-                    command=lambda: self.display_item(Potion.generate_potion, 'Potion', 'red_bg', self.__item_labels['Potion'])),
-            "Ring":tk.Button(self.__option_frame, text='Generate Ring',
-                    command=lambda: self.display_item(Ring.generate_ring, 'Ring', 'orange_bg', self.__item_labels['Ring'])),
-            "Scroll":tk.Button(self.__option_frame, text='Generate Scroll',
-                    command=lambda: self.display_item(Scroll.generate_scroll, 'Scroll', 'yellow_bg',self.__item_labels['Scroll'])),
-            "Wand":tk.Button(self.__option_frame, text='Generate Rod/Staff/Wand',
-                    command=lambda: self.display_item(Wand.generate_wand, 'Wand', 'green_bg', self.__item_labels['Wand'])),
-            "Armor":tk.Button(self.__option_frame, text='Generate Armor',
-                    command=lambda: self.display_item(Armor.generate_armor, 'Armor', 'blue_bg', self.__item_labels['Armor'])),
-            "Clear":tk.Button(self.__option_frame, text='Clear', 
-                    command=lambda: self.clear_text())
-        }
-
-        for button in self.__buttons:
-            self.__buttons[button].pack(side='left', padx=5)
+        #****************************************************************************************
 
         tk.mainloop()
 
-    def clear_text(self):
-        self.__display_text.config(state=tk.NORMAL)
-        self.__display_text.delete("1.0", tk.END)
-        self.__display_text.config(state=tk.DISABLED)
+    def create_buttons(self):
+        self.buttons = {
+            "Potion":tk.Button(self.option_frame, text='Generate Potion', 
+                    command=lambda: self.display_item(Potion.generate_potion, 'Potion', 'red_bg', self.labels['Potion'])),
+            "Ring":tk.Button(self.option_frame, text='Generate Ring',
+                    command=lambda: self.display_item(Ring.generate_ring, 'Ring', 'orange_bg', self.labels['Ring'])),
+            "Scroll":tk.Button(self.option_frame, text='Generate Scroll',
+                    command=lambda: self.display_item(Scroll.generate_scroll, 'Scroll', 'yellow_bg',self.labels['Scroll'])),
+            "Wand":tk.Button(self.option_frame, text='Generate Rod/Staff/Wand',
+                    command=lambda: self.display_item(Wand.generate_wand, 'Wand', 'green_bg', self.labels['Wand'])),
+            "Armor":tk.Button(self.option_frame, text='Generate Armor',
+                    command=lambda: self.display_item(Armor.generate_armor, 'Armor', 'blue_bg', self.labels['Armor'])),
+            "Clear":tk.Button(self.option_frame, text='Clear', 
+                    command=lambda: self.clear_text())
+        }
 
-        for count in self.__item_counts:
-            self.__item_counts[count] = 0
+        for button in self.buttons:
+            self.buttons[button].pack(side='left', padx=5)
 
-        self.__item_labels['Total'].config(text=f'Number of Items: {self.__item_counts["Total"]}')
-        self.__item_labels['Potion'].config(text=f'Number of Potions: {self.__item_counts["Potion"]}')
-        self.__item_labels['Ring'].config(text=f'Number of Rings: {self.__item_counts["Ring"]}')
-        self.__item_labels['Scroll'].config(text=f'Number of Scrolls: {self.__item_counts["Scroll"]}')
-        self.__item_labels['Wand'].config(text=f'Number of Wands: {self.__item_counts["Wand"]}')
-        self.__item_labels['Armor'].config(text=f'Number of Armor: {self.__item_counts["Armor"]}')
+    def create_labels(self):
+        self.label_vars = {
+            'Total':tk.StringVar(),
+            'Potion':tk.StringVar(),
+            'Ring':tk.StringVar(),
+            'Scroll':tk.StringVar(),
+            'Wand':tk.StringVar(),
+            'Armor':tk.StringVar()
+        }
 
-    def display_item(self, generate: callable, name: str, bg: str, box: tk.Label) -> None:
+        self.labels = {
+            'Total':tk.Label(self.result_frame, textvariable=self.label_vars['Total']),
+            'Potion':tk.Label(self.result_frame, textvariable=self.label_vars['Potion']),
+            'Ring':tk.Label(self.result_frame, textvariable=self.label_vars['Ring']),
+            'Scroll':tk.Label(self.result_frame, textvariable=self.label_vars['Scroll']),
+            'Wand':tk.Label(self.result_frame, textvariable=self.label_vars['Wand']),
+            'Armor':tk.Label(self.result_frame, textvariable=self.label_vars['Armor'])
+        }
+
+        for label in self.labels:
+            self.labels[label].pack()
+
+    def create_text(self):
+        self.display_text = scrolledtext.ScrolledText(self.display_frame, height=30, width=100)
+        self.display_font = ("Courier", 10, "bold")
+        self.display_tags = {
+            'red_bg':'#ff886e',
+            'orange_bg':'#ffb46e',
+            'yellow_bg':'#f8ff6e',
+            'green_bg':'#80ff6e',
+            'blue_bg':'#6e84ff'
+        }
+
+        for key, value in self.display_tags.items():
+            self.display_text.tag_configure(key, background=value, font=self.display_font)
+        del self.display_tags
+        del self.display_font
+
+        self.display_text.config(state=tk.DISABLED)
+        self.display_text.pack()
+
+    def display_item(self, generate, name, tag, box):
         item = generate()
-        self.__display_text.config(state=tk.NORMAL)
-        self.__display_text.insert(tk.END, item, bg)
-        self.__display_text.config(state=tk.DISABLED)
+        self.display_text.config(state=tk.NORMAL)
+        self.display_text.insert(tk.END, item, tag)
+        self.display_text.config(state=tk.DISABLED)
 
-        self.__item_counts['Total'] += 1
-        self.__item_labels['Total'].config(text=f'Number of Items: {self.__item_counts['Total']}')
-        self.__item_counts[name] += 1
-        box.config(text=f'Number of {name}: {self.__item_counts[name]}')
+        self.counts['Total'] += 1
+        self.label_vars['Total'].set(f'Number of Total Items: {self.counts['Total']}')
+
+        self.counts[name] += 1
+        self.label_vars[name].set(f'Number of {name}: {self.counts[name]}')
+
+    def clear_text(self):
+        self.display_text.config(state=tk.NORMAL)
+        self.display_text.delete("1.0", tk.END)
+        self.display_text.config(state=tk.DISABLED)
+
+        for count in self.counts:
+            self.counts[count] = 0
+
+        for key in self.label_vars:
+            self.label_vars[key].set(f'Number of {key}: {self.counts[key]}')
